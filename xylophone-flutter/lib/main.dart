32cc7e8e659bae474f8a34b2e6474345dd520d55
Arsh-78 <arshbz53@gmail.com>
2022-04-20 01:18:32 +0530

    Completed Xylophone

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -4,68 +4,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int a) {
+    final player = AudioCache();
+    player.play('note$a.wav');
+  }
+
+  Expanded buildKey({C olor color, int sn}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playSound(sn);
+        },
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(children: [
-            FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text("Click Me"),
-              color: Colors.amber,
-            ),
-            FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note2.wav');
-              },
-              child: Text("Click Me"),
-              color: Colors.blue,
-            ),
-            FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note3.wav');
-              },
-              child: Text("Click Me"),
-              color: Colors.teal,
-            ),
-            FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note4.wav');
-              },
-              child: Text("Click Me"),
-              color: Colors.cyan,
-            ),
-            FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note5.wav');
-              },
-              child: Text("Click Me"),
-              color: Colors.white60,
-            ),
-            FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note6.wav');
-              },
-              child: Text("Click Me"),
-              color: Colors.black38,
-            ),
-            FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note7.wav');
-              },
-              child: Text("Click Me"),
-              color: Colors.transparent,
-            ),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            buildKey(color: Colors.red, sn: 1),
+            buildKey(color: Colors.blue, sn: 2),
+            buildKey(color: Colors.green, sn: 3),
+            buildKey(color: Colors.yellow, sn: 4),
+            buildKey(color: Colors.orange, sn: 5),
+            buildKey(color: Colors.pink, sn: 6),
+            buildKey(color: Colors.purple, sn: 7),
           ]),
         ),
       ),
