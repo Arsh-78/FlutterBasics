8e04ae9f43e237472029934e6a938bc832f666c1
Arsh-78 <arshbz53@gmail.com>
2022-04-19 21:57:09 +0530

    all files

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -9,15 +9,64 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
+          child: Column(children: [
+            FlatButton(
               onPressed: () {
                 final player = AudioCache();
                 player.play('note1.wav');
               },
               child: Text("Click Me"),
+              color: Colors.amber,
             ),
-          ),
+            FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note2.wav');
+              },
+              child: Text("Click Me"),
+              color: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note3.wav');
+              },
+              child: Text("Click Me"),
+              color: Colors.teal,
+            ),
+            FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note4.wav');
+              },
+              child: Text("Click Me"),
+              color: Colors.cyan,
+            ),
+            FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note5.wav');
+              },
+              child: Text("Click Me"),
+              color: Colors.white60,
+            ),
+            FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note6.wav');
+              },
+              child: Text("Click Me"),
+              color: Colors.black38,
+            ),
+            FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note7.wav');
+              },
+              child: Text("Click Me"),
+              color: Colors.transparent,
+            ),
+          ]),
         ),
       ),
     );
